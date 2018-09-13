b44177643c20139a0a79dfa10ebecc2e07286905
Stu Sherwin <stu.sherwin@gmail.com>
2018-09-13 19:41:23 +0100

    More pages/better nav logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import '../static/dist/styles.css';
 
-alert('Hi there!');
+function showCurrentTab() {
+  document.querySelectorAll('.tab-content').forEach(function(e) {
+    e.style.display = 'none';
+  })
+
+  var elem = document.getElementById(location.hash.substr(1) || 'about')
+  elem.style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", showCurrentTab);
+window.onhashchange = showCurrentTab;
