a253bf878801a38ecb259e9873b6a9b420eee8ad
Stu Sherwin <stu.sherwin@gmail.com>
2020-03-22 05:52:22 +0000

    Temporary duplicate of WordPress site to get press release up

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,17 @@
 function showCurrentTab() {
-  document.querySelectorAll('.tab-content').forEach(function(e) {
-    e.style.display = 'none';
-    document.querySelectorAll('[data-href="' + e.id + '"]').forEach(function(link) {
-      link.classList.remove('font-normal', 'pointer-events-none', 'cursor-default');
-    })
-  })
+  // document.querySelectorAll('.tab-content').forEach(function(e) {
+  //   e.style.display = 'none';
+  //   document.querySelectorAll('[data-href="' + e.id + '"]').forEach(function(link) {
+  //     link.classList.remove('font-normal', 'pointer-events-none', 'cursor-default');
+  //   })
+  // })
 
-  var elem = document.getElementById(location.hash.substr(1) || 'faq');
-  elem.style.display = 'block';
-  document.querySelectorAll('[data-href="' + elem.id + '"]').forEach(function(link) {
-    link.classList.add('font-normal', 'pointer-events-none', 'cursor-default');
-    link.blur();
-  })
+  // var elem = document.getElementById(location.hash.substr(1) || 'faq');
+  // elem.style.display = 'block';
+  // document.querySelectorAll('[data-href="' + elem.id + '"]').forEach(function(link) {
+  //   link.classList.add('font-normal', 'pointer-events-none', 'cursor-default');
+  //   link.blur();
+  // })
 }
 
 document.addEventListener("DOMContentLoaded", showCurrentTab);
